b0febe54087ba8b3feca15a7085d01cbdfa45503
Heike Miersch <heike.miersch@hotmail.de>
2019-11-21 17:23:31 +0100

    after the chaos

--- second.js
+++ second.js
@@ -5,20 +5,21 @@ console.log(firebase);
 
 let username = "";
 let loggedIn = false;
+let signedIn = false;
 // google sign in
 // create an instance of the google provider object (sign in with google)
 let provider = new firebase.auth.GoogleAuthProvider();
 // initialize cloud firestore through firebase
 let database = firebase.firestore();
 
-function login() {
+function loginGoogle() {
   // to sign in with a pop-up window, call signinwithpopup
   firebase
     .auth()
     .signInWithPopup(provider)
-    .then(function(result) {
+    .then(function (result) {
       // The signed-in user info.
-      var user = result.user;
+      let user = result.user;
       // all that she wants
       username = user.displayName;
       console.log("Hello ", username);
@@ -26,31 +27,91 @@ function login() {
       readComments();
       // ...
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // Handle Errors here.
       console.log("error", error);
     });
 }
 
-function writeComment() {
-  let message = document.getElementById("message").value;
-  console.log("message", message);
-  let date = new Date();
-  let timestamp = date.getTime();
-  database
-    .collection("comments")
-    .add({
-      message: message,
-      name: username,
-      date: date
-    })
-    .then(function(docRef) {
-      console.log("Document written with ID: ", docRef.id);
-      readComments();
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
+function createAccount() {
+  let newEmail = document.getElementById("signupEmail").value;
+  let newPassword = document.getElementById("signupPassword").value;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newEmail, newPassword)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
     });
+
+  // readComments();
+  // modal.style.display = "none";
+  isUser();
+}
+
+
+// Get the modal
+var modal = document.getElementById("loginPasswordModal");
+// Get the button that opens the modal
+var btn = document.getElementById("modalButton");
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+function loginPassword() {
+  let userEmail = document.getElementById("loginEmail").value;
+  let userPassword = document.getElementById("loginPassword").value;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(userEmail, userPassword).then(() => isUser()).catch(function (error) {
+      // Handle Errors here.
+      alert("No valid entry!")
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  // readComments();
+}
+
+function isUser() {
+  console.log("isUser called")
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log(user)
+      // signedIn = true;
+      // loggedIn = true;
+      modal.style.display = "none";
+      readComments();
+    }
+  });
+}
+
+function writeComment() {
+  if (loggedIn == true) {
+    let message = document.getElementById("message").value;
+    // console.log("message", message);
+    let date = new Date();
+    // let timestamp = date.getTime();
+    database
+      .collection("comments")
+      .add({
+        message: message,
+        name: username,
+        date: date,
+        Name: userEmail
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+        readComments();
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  }
 }
 
 function readComments() {
@@ -71,15 +132,19 @@ function readComments() {
 
         let documents = doc.data();
         console.log(documents);
-        let { name, message, date } = documents;
-        user.innerHTML = name;
+        let {
+          name,
+          message,
+          date
+        } = documents;
+        user.innerHTML = "(" + name + ")";
         comment.innerHTML = message;
         // dateP.innerHTML = date;
         // console.log(date);
         // console.log(user);
         // console.log(message);
         container.appendChild(comment);
-        container.appendChild(user);
+        comment.appendChild(user);
         // container.appendChild(dateP);
         chat.appendChild(container);
       });
@@ -90,12 +155,12 @@ function logout() {
   firebase
     .auth()
     .signOut()
-    .then(function() {
+    .then(function () {
       // Sign-out successful.
       console.log("Logout successful!");
       loggedIn = false;
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // An error happened.
     });
 }
